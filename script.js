da4fb6f9188281dc3b83f7a9dc5edbfbd1942279
mercedesaven <mercedes.aven@gmail.com>
2020-07-29 02:03:54 -0300

    wip:productos, filpcard

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-   /*  document.getElementById("mainHeader").style.padding = "30px 10px"; */
-    document.getElementById("mainHeader").style.height = "40px";
+  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    document.getElementById("mainHeader").style.height = "70px";
     document.getElementById("navBar").style.margin = "8px";
   } else {
-   /*  document.getElementById("mainHeader").style.padding = "10px 10px"; */
-    document.getElementById("mainHeader").style.height = "80px";
+    document.getElementById("mainHeader").style.height = "120px";
     document.getElementById("navBar").style.margin = "30px";
   }
 }
